43c1450f629c7fcfb1b2f98c7469cd265aa49597
bryanaguiar <74388707+bryanaguiar@users.noreply.github.com>
2022-06-07 21:05:43 -0700

    Assignment 6 OAuth 2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import './App.css';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import SchedList from './components/SchedList';
-import Semester from './components/Semester';
-import Admin from './components/Admin';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import "./App.css";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import SchedList from "./components/SchedList";
+import Semester from "./components/Semester";
+import Admin from "./components/Admin";
+import Login from "./components/Login";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <AppBar position="static" color="default">
         <Toolbar>
-           <Typography variant="h6" color="inherit">
+          <Typography variant="h6" color="inherit">
             Course Registration
-           </Typography>
+          </Typography>
         </Toolbar>
       </AppBar>
       <BrowserRouter>
-       <Switch>
-        <Route exact path='/' component={Semester} />
-        <Route path='/schedule' component={SchedList} />
-        <Route path='/admin' component={Admin}/>
-       </Switch>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/semester" component={Semester} />
+          <Route path="/schedule" component={SchedList} />
+          <Route path="/admin" component={Admin} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
